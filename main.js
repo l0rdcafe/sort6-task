61982fac0f5f5d27cf299b6106719df11d180b96
Ismail Arafa <ismailarafa18@gmail.com>
2017-12-14 03:40:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+var model = {};
+var view = (function () {
+  var render = function (addString) {
+    var div = document.getElementById('addresses');
+    var p = document.createElement('p');
+    p.innerHTML = addString;
+
+    div.appendChild(p);
+  };
+
+  return {
+    render: render
+  };
+})();
+
+var handlers = (function () {
+  var showAddresses = function () {
+    var sequence = Promise.resolve();
+
+    model.places.forEach(function (place) {
+      sequence = sequence.then(function () {
+        return GoogleAPI.getAddress(place);
+      })
+      .then(view.render)
+      .catch(function (error) {
+        console.log(error);
+      });
+    });
+  };
+
+  return {
+    showAddresses: showAddresses
+  }
+})();
+
+var GoogleAPI = (function () {
+  var API_KEY = 'AIzaSyBzrCI1GVyEBLM6nZfgjoRmiJy4fQ0VqRQ';
+  var GOOGLE_URL = 'http://maps.googleapis.com/maps/api/geocode/';
+
+  var getAddress = function (address) {
+    return fetch(GOOGLE_URL + 'json?latlng=' + address.lat + ',' + address.long + '&sensor=true')
+      .then(function (req) {
+        return req.json();
+      })
+      .then(function (json) {
+        return json.results[0].formatted_address;
+      })
+      .catch(function (err) {
+        return Promise.reject(err);
+      });
+  };
+  return {
+    getAddress: getAddress
+  };
+})();
+
+model.places = [
+    {
+        long: 31.2225712,
+        lat: 30.0627897
+    },
+    {
+        long: 31.237592,
+        lat: 30.075882
+    },
+    {
+        long: 31.198710,
+        lat: 30.070682
+    },
+    {
+        long: 30.991285,
+        lat: 30.052664
+    },
+    {
+        long: 30.936577,
+        lat: 29.966919
+    },
+    {
+        long: 29.881227,
+        lat: 31.208073
+    },
+    {
+        long: 29.935524,
+        lat: 31.219805
+    }
+];
+
+document.addEventListener('DOMContentLoaded', handlers.showAddresses, false);
